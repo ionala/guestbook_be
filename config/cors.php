ff8264c69eaf2f4b0ad5bcfdca4e0f21feb7fe2e
Viona <vionarosalia58@gmail.com>
2025-11-30 19:48:37 +0700

    update cors settings

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'], // frontend Next.js kamu
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
